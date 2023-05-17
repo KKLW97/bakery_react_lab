5c73e0645046ab2e030cbc609988a911ca61d7db
Kelly Wong <kellykl_wong@hotmail.com>
2023-05-17 18:53:02 +0100

    adds background colour to body in App.css

--- src/cake/Cake.js
+++ src/cake/Cake.js
@@ -1,17 +1,16 @@
 const Cake = ({cake}) => {
   
   return (
-    <>
-        <h2>{cake.cakeName}</h2>
-        <ul>
-          {cake.ingredients.map((ingredient, index) => (
-            <li id={index}>{ingredient}</li>))}
-        </ul>
-        {/* <p>Ingredients: {cake.ingredients}</p> */}
-        <p>Price: {cake.price}</p>
-        <p>Rating: {cake.rating}</p>
-
-    </>
+    <div>
+      <h2>{cake.cakeName}</h2>
+      <ul>
+        {cake.ingredients.map((ingredient, index) => (
+          <li id={index}>{ingredient}</li>))}
+      </ul>
+      {/* <p>Ingredients: {cake.ingredients}</p> */}
+      <p>Price: {cake.price}</p>
+      <p>Rating: {cake.rating}</p>
+    </div>
   )
 }
 
